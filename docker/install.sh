a705b76fdefcc9a26aff658e6592c55292493c6b
Phil Proctor <philliptproctor@gmail.com>
2020-11-18 22:11:37 -0500

    version tag included prefix twice

--- docker/install.sh
+++ docker/install.sh
@@ -2,7 +2,7 @@
 SYSTEM_ARCH="$(uname -m)"
 CORVUS_VERSION="$1"
 CORVUS_FILENAME="corvus-${SYSTEM_ARCH}-unknown-linux-musl.tar.xz"
-CORVUS_URL="https://github.com/proctorlabs/corvus/releases/download/v${CORVUS_VERSION}/${CORVUS_FILENAME}"
+CORVUS_URL="https://github.com/proctorlabs/corvus/releases/download/${CORVUS_VERSION}/${CORVUS_FILENAME}"
 
 mkdir -p /dist
 apk add curl ca-certificates tar xz
