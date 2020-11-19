a453b5733522a96848e27e12fc8ae2763768d71b
Phil Proctor <philliptproctor@gmail.com>
2020-11-18 22:51:39 -0500

    simplify filenames for simpler install scripts

--- docker/install.sh
+++ docker/install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 SYSTEM_ARCH="$(uname -m)"
 CORVUS_VERSION="$1"
-CORVUS_FILENAME="corvus-${SYSTEM_ARCH}-unknown-linux-musl.tar.xz"
+CORVUS_FILENAME="corvus.${SYSTEM_ARCH}.tar.xz"
 CORVUS_URL="https://github.com/proctorlabs/corvus/releases/download/${CORVUS_VERSION}/${CORVUS_FILENAME}"
 
 mkdir -p /dist
